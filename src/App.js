695a111ac54c7855c3f8978b8fcf1faa65a40dee
Kyle Speller <kspell22@gmail.com>
2022-07-08 04:28:24 -0700

    Added main App file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Card } from 'react-bootstrap';
+import NavBar from './NavBar';
+import TaskItem from './TaskItem';
+import NewTaskForm from "./NewTaskForm";
+import UpdateTaskForm from "./UpdateTaskForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import templateTasks from "./templateTasks";
 
 function App() {
+  const [taskList, setTaskList] = React.useState(templateTasks);
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [currentEditTask, setCurrentEditTask] = React.useState("");
+  const [currentEditIndex, setCurrentEditIndex] = React.useState("");
+  const [currentEditName, setCurrentEditName] = React.useState("");
+  const [currentEditTime, setCurrentEditTime] = React.useState("");
+  const [currentEditDescription, setCurrentEditDescription] = React.useState("");
+  
+  const addTask = newTask => {
+    const newTaskList = [...taskList, newTask];
+    setTaskList(newTaskList);
+  };
+
+  const markTask = index => {
+    const newTaskList = [...taskList];
+    newTaskList[index].isDone = !taskList[index].isDone;
+    setTaskList(newTaskList);
+  };
+
+  const editTask = index => {
+    setCurrentEditIndex(index);
+    setIsEditing(!isEditing);
+    setCurrentEditTask(taskList[index]);
+    setCurrentEditName(taskList[index].title);
+    setCurrentEditTime(taskList[index].time);
+    setCurrentEditDescription(taskList[index].description);
+  }
+
+  const updateTask = newTask => {
+    const newTaskList = [...taskList];
+    newTaskList[currentEditIndex] = newTask;
+    setTaskList(newTaskList);
+  }
+
+  const setTaskName = name => {
+    setCurrentEditName(name)
+  }
+
+  const setTaskTime = time => {
+    setCurrentEditTime(time)
+  }
+
+  const setTaskDescription = description => {
+    setCurrentEditDescription(description)
+  }
+
+  const closeEditForm = () => {
+    setIsEditing(!isEditing);
+  }
+
+  const removeTask = index => {
+    const newTaskList = [...taskList];
+    newTaskList.splice(index, 1);
+    setTaskList(newTaskList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar/>
+      <div className="container">
+        <NewTaskForm addTask={addTask} isEditing={isEditing}/>
+        <div className="task-list">
+          {taskList.map((task, index) => (
+            <Card className="task-card">
+              <Card.Body>
+                <TaskItem
+                  key={index}
+                  index={index}
+                  task={task}
+                  markTask={markTask}
+                  removeTask={removeTask}
+                  editTask={editTask}
+                  isEditing={isEditing}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+        <UpdateTaskForm 
+          isEditing={isEditing}
+          currentEditTask={currentEditTask}
+          currentEditName={currentEditName}
+          currentEditTime={currentEditTime}
+          currentEditDescription={currentEditDescription}
+          setTaskName={setTaskName}
+          setTaskTime={setTaskTime}
+          setTaskDescription={setTaskDescription}
+          updateTask={updateTask}
+          closeEditForm={closeEditForm}
+        />
+      </div>
     </div>
   );
 }
